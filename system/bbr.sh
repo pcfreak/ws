814ede74c3ef908a4d500618424c73f06f6e1f43
pcfreak <68300186+pcfreak@users.noreply.github.com>
2023-03-25 17:25:49 +0200

    Update bbr.sh

--- system/bbr.sh
+++ system/bbr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Optimasi Speed By Virtual
 #wget https://github.com/${GitUser}/
-GitUser="akuhaa021"
+GitUser="pcfreak"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
