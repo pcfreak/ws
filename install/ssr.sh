78e06106d579d6fd349ec241a18eff394b8361b5
pcfreak <68300186+pcfreak@users.noreply.github.com>
2023-03-25 17:48:31 +0200

    Update ssr.sh

--- install/ssr.sh
+++ install/ssr.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 #wget https://github.com/${GitUser}/
-GitUser="akuhaa021"
+GitUser="pcfreak"
 
 sh_ver="1.0.26"
 filepath=$(cd "$(dirname "$0")"; pwd)
